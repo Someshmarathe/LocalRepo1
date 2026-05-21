aa572f6db3e99dedfc5eb83fa60d9459b060e00a
someshmarathe <someshmarathe00@gamil.com>
2026-05-22 01:10:15 +0530

    add new line

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ package firstJavaCode;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello from git");
+        System.out.println("add new line");
     }
 }
